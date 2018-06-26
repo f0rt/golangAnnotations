d6c22736176b0e8e59d203497d27d025f885d385
Marc Grol <mgrol@xebia.com>
2018-06-26 16:43:41 +0200

    Remove name field for outbound

--- generator/outbound/outboundAnnotation/outboundAnnotations_test.go
+++ generator/outbound/outboundAnnotation/outboundAnnotations_test.go
@@ -10,9 +10,8 @@ import (
 func TestCorrectOutboundtServiceAnnotation(t *testing.T) {
 	registry := annotation.NewRegistry(Get())
 
-	ann, ok := registry.ResolveAnnotationByName([]string{`// @OutboundClient( name = "myClient", description = "my descr", external = "true", http = "true")`}, "OutboundClient")
+	ann, ok := registry.ResolveAnnotationByName([]string{`// @OutboundClient( description = "my descr", external = "true", http = "true")`}, "OutboundClient")
 	assert.True(t, ok)
-	assert.Equal(t, "myClient", ann.Attributes["name"])
 	assert.Equal(t, "my descr", ann.Attributes["description"])
 	assert.Equal(t, "true", ann.Attributes["http"])
 	assert.Equal(t, "true", ann.Attributes["external"])
@@ -27,9 +26,8 @@ func TestIncompleteOutboundServiceAnnotation(t *testing.T) {
 func TestCorrectOutboundOperationAnnotation(t *testing.T) {
 	registry := annotation.NewRegistry(Get())
 
-	a, ok := registry.ResolveAnnotation(`// @OutboundOperation( name = "myOper", description = "my descr", inbound = "true", outbound = "false" )`)
+	a, ok := registry.ResolveAnnotation(`// @OutboundOperation( description = "my descr", inbound = "true", outbound = "false" )`)
 	assert.True(t, ok)
-	assert.Equal(t, "myOper", a.Attributes["name"])
 	assert.Equal(t, "my descr", a.Attributes["description"])
 	assert.Equal(t, "true", a.Attributes["inbound"])
 	assert.Equal(t, "false", a.Attributes["outbound"])
